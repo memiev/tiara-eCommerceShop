923bd6d7fa435e311e297fb227a0a7863380fbde
DENIS MEMIEV <denismemiev@gmail.com>
2022-02-23 11:00:30 +0200

    adding header components and links to homepage

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/tiara.svg'
+import './header.styles.scss'
+export const Header = () => (
+    <div className="header">
+        <Link className='logo-container' to='/'>
+            <Logo className='logo'/>
+        </Link>
+        <div className="options">
+            <Link className="option" to='/shop'>SHOP</Link>
+            <Link className="option" to='/shop'>CONTACT</Link>
+        </div>
+    </div>
+)
